210075c6a28ef27449a84138b5549ca6c8728446
Suraj Kumar <suraj110905@gmail.com>
2026-01-19 03:10:47 +0530

    updates

--- Day_1_Lecture_2_17-01-2026/print_counting_1-N.cpp
+++ Day_1_Lecture_2_17-01-2026/print_counting_1-N.cpp
@@ -1,1 +1,11 @@
 //Q7. Print Counting from 1 to N
+#include <iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"enter the number";
+    cin>>num;
+    for(int i=1;i<=num;i++){
+        cout<<i;
+    }
+}
--- Day_1_Lecture_2_17-01-2026/sum_n_natural_numbers.cpp
+++ Day_1_Lecture_2_17-01-2026/sum_n_natural_numbers.cpp
@@ -1,1 +1,10 @@
 //Q8. Find the Sum of N Natural Numbers
+#include <iostream>
+using namespace std;
+int main(){
+    int n,sum;
+    cout<<"enter the number";
+    cin>>n;
+    sum = n*(n+1)/2;
+    cout<<"sum of n natural numbers is : "<<sum;
+}
