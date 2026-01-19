7ec3ee19761fd9c2e385681014203f79f4387967
Suraj Kumar <suraj110905@gmail.com>
2026-01-20 01:40:16 +0530

    sum of digits

--- Day_1_Lecture_2_17-01-2026/sumofdigits_of_givenno_usingforloop.cpp
+++ Day_1_Lecture_2_17-01-2026/sumofdigits_of_givenno_usingforloop.cpp
@@ -1,1 +1,16 @@
 //Q12. Find the Sum of Digits of a Given Number using a Loop
+#include <iostream>
+using namespace std;
+int main(){
+    int n,sum=0;
+    cout<<"enter the number";
+    cin>>n;
+    if(n<10){
+        cout<<"enter a valid number i.e > 9";
+    }else{
+        for(;n>0;n=n/10){ //initalization of value is done so it is left blank
+            sum = sum + n%10;          
+        } 
+        cout<<"sum of digits is : "<<sum;
+    }
+}
