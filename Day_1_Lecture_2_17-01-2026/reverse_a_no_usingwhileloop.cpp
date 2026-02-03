e11ca39f8ad7ec8fca5760de035c03fd15d447cf
Suraj Kumar <suraj110905@gmail.com>
2026-02-03 23:36:45 +0530

    updates

--- Day_1_Lecture_2_17-01-2026/reverse_a_no_usingwhileloop.cpp
+++ Day_1_Lecture_2_17-01-2026/reverse_a_no_usingwhileloop.cpp
@@ -2,5 +2,14 @@
 #include <iostream>
 using namespace std;
 int main(){
+    int num,rev=0;
+    cin>>num;
+    
+    for(;num!=0;num=num/10){
+        int digit = num%10;
+        rev=rev*10+digit;
+    }
+    cout<<rev;
+
     
 }
