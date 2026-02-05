531ccec683296fbf75c816f5f04fe7c8c8892dee
Suraj Kumar <suraj110905@gmail.com>
2026-02-06 00:06:18 +0530

    updates in code

--- Day_2_Lecture_3_20-01-2026/patternrough.cpp
+++ Day_2_Lecture_3_20-01-2026/patternrough.cpp
@@ -1,5 +1,20 @@
-# include <iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int binary,decimal=0,
+
+int main() {
+    int n,a,b;
+    cin >> n;   // example: 378
+    a=0;
+
+    
+
+    for (int i = 2; i <= n; i++) {
+        while (n % i == 0) {
+            cout << i << " ";
+            n = n / i;
+        }
+    }
+    b= a+i;
+
+    return 0;
 }
