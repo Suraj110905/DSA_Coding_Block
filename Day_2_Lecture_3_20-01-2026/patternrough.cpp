b2c4db8a5df1d6114cf2fcf98404ebf74db6f252
Suraj Kumar <suraj110905@gmail.com>
2026-02-07 23:42:47 +0530

    updates

--- Day_2_Lecture_3_20-01-2026/patternrough.cpp
+++ Day_2_Lecture_3_20-01-2026/patternrough.cpp
@@ -1,20 +1,13 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    int n,a,b;
-    cin >> n;   // example: 378
-    a=0;
-
-    
-
-    for (int i = 2; i <= n; i++) {
-        while (n % i == 0) {
-            cout << i << " ";
-            n = n / i;
-        }
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
     }
-    b= a+i;
-
-    return 0;
+    for(int i=0;i<n;i++){
+        cout<<arr[i];
+    }
 }
