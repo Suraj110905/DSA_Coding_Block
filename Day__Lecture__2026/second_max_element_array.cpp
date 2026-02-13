2d58e91a18f78413dc7e52c0f2e3d43636d40491
Suraj Kumar <suraj110905@gmail.com>
2026-02-13 11:00:36 +0530

    code push

--- Day__Lecture__2026/second_max_element_array.cpp
+++ Day__Lecture__2026/second_max_element_array.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 #include <climits>
+#include<algorithm>
 using namespace std;
 int main(){
-    int arr[5] = {1,9,2,3,4,};
-    int ele = INT_MIN;
-    for(int i=0;i<3;i++){
-        if(arr[i]>ele){
-            ele = arr[i];
+    int arr[5] = {1,9,2,3,4};
+    //part 1 find largest element
+    int largest = INT_MIN;
+    int second = INT_MIN;
+    for(int i=0;i<5;i++){
+        if(arr[i]>largest){
+            largest = arr[i];
+        }
+    } 
+    //second part find second largest
+    for(int i=0;i<5;i++){
+        if(largest!=arr[i] 
+        ){
+            second = max(second,arr[i]);
         }
     }
-    for(int i = 0;i<5;i++){
-        cout<<arr[i];
-    }
+    cout<<second;
 }
